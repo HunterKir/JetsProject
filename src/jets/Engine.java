fc3cca35b2ae104481e730b4c29fd234b590e4e2
HunterKir <hunter.kiritsis@gmail.com>
2017-10-08 11:28:29 -0600

    populated pilots array, added pilot management to menu

--- src/jets/Engine.java
+++ src/jets/Engine.java
@@ -5,12 +5,28 @@ import java.util.Scanner;
 public class Engine {
 	static Scanner kb = new Scanner(System.in);
 	static Hangar alpha = new Hangar("alpha");
+	static Pilot pilots[] = new Pilot[15];
 	public static void main(String[] args) {
 		alpha.setFleet(0, 439, 18.8, 290, "A-10 Thunderbolt II");
 		alpha.setFleet(1, 1500, 150.0, 529, "F-22 Raptor");
 		alpha.setFleet(2, 1875, 31.3, 790, "F-15E Strike Eagle");
 		alpha.setFleet(3, 630, 737.0, 6900, "B-2 Spirit");
 		alpha.setFleet(4, 300, 132.4, 2530, "AC-130");
+		pilots[0] = new Pilot("Hunter");
+		pilots[1] = new Pilot("Alex");
+		pilots[2] = new Pilot("Jake");
+		pilots[3] = new Pilot("Greg");
+		pilots[4] = new Pilot("Jason");
+		pilots[5] = new Pilot("Jen");
+		pilots[6] = new Pilot("Aaron");
+		pilots[7] = new Pilot("Stumpe");
+		pilots[8] = new Pilot("Dave");
+		pilots[9] = new Pilot("Alexis");
+		pilots[10] = new Pilot("Rosanne");
+		pilots[11] = new Pilot("Chris");
+		pilots[12] = new Pilot("Airik");
+		pilots[13] = new Pilot("Ekubay");
+		pilots[14] = new Pilot("Rob");
 		while (true) {
 			printMenu();
 			menuSelect(inputFilter(kb.next()));
@@ -23,7 +39,8 @@ public class Engine {
 		System.out.println("|   [2] View Fastest Jet            |");
 		System.out.println("|   [3] View Jet w/ Longest Range   |");
 		System.out.println("|   [4] Add a Jet                   |");
-		System.out.println("|   [5] Exit                        |");
+		System.out.println("|   [5] Manage Pilots               |");
+		System.out.println("|   [6] Exit                        |");
 		System.out.println("|-----------------------------------|");
 	}
 	
@@ -51,6 +68,9 @@ public class Engine {
 				loop = false;
 			}
 			else if (num == 5) {
+				//manage pilots
+			}
+			else if (num == 6) {
 				System.out.println("Exiting Program.");
 				System.exit(0);
 				loop = false;
